5e3bdb32f09c36801623a84ada796f8736b7efe5
Juanito <juanprieto1627@gmail.com>
2025-08-13 16:06:36 -0500

    v0.2_clasesPaqueteModelo_Juanito_130082025

--- src/main/java/com/sena/ecommerce/model/producto.java
+++ src/main/java/com/sena/ecommerce/model/producto.java
@@ -1,11 +1,19 @@
 package com.sena.ecommerce.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+
 @Entity
 @Table(name = "prductos")
+
 public class producto {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 
 	private Integer id;
 	private String nombre;
@@ -15,9 +23,83 @@ public class producto {
 	private Integer cantidad;
 	
 	@ManyToOne
-	private Usuario usuario;
+	private usuario usuario;
 	
-	public Producto() {
+	public producto() {
 		
 	}
+
+	public producto(Integer id, String nombre, String dscripcion, String imagen, Double precio) {
+		super();
+		this.id = id;
+		this.nombre = nombre;
+		this.dscripcion = dscripcion;
+		this.imagen = imagen;
+		this.precio = precio;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getDscripcion() {
+		return dscripcion;
+	}
+
+	public void setDscripcion(String dscripcion) {
+		this.dscripcion = dscripcion;
+	}
+
+	public String getImagen() {
+		return imagen;
+	}
+
+	public void setImagen(String imagen) {
+		this.imagen = imagen;
+	}
+
+	public Double getPrecio() {
+		return precio;
+	}
+
+	public void setPrecio(Double precio) {
+		this.precio = precio;
+	}
+
+	public Integer getCantidad() {
+		return cantidad;
+	}
+
+	public void setCantidad(Integer cantidad) {
+		this.cantidad = cantidad;
+	}
+
+	public usuario getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(usuario usuario) {
+		this.usuario = usuario;
+	}
+
+	@Override
+	public String toString() {
+		return "producto [id=" + id + ", nombre=" + nombre + ", dscripcion=" + dscripcion + ", imagen=" + imagen
+				+ ", precio=" + precio + ", cantidad=" + cantidad + "]";
+	}
+	
+	
+	
 }
